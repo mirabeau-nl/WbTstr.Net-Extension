02a66159aae80d147f18f0ef8add9db549ce19a9
Onno Valkering <ovalkering@mirabeau.nl>
2016-01-07 15:24:03 +0100

    Added initial SpecFlow project template

---
+++ Source/WbTstrNet.SpecFlowProjectTemplate/Pages/Common/HomePage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$
+using FluentAutomation;
+
+using $safeprojectname$.Pages;
+
+namespace $safeprojectname$.Pages.Common
+{
+    public class HomePage : BasePage<HomePage>
+    {
+        private const string Navigation = "#nav";
+        private const string NavContactLink = "#nav > li:nth-child(4)";
+
+        public HomePage(FluentTest test) : base(test)
+        {
+            Url = "http://www.mirabeau.nl";
+            At = () => I.Assert.Visible(Navigation);
+        }
+
+        public HomePage ClickNavContactLink()
+        {
+            I.Click(NavContactLink);
+
+            return this;
+        }
+    }
+}
---
+++ Source/WbTstrNet.SpecFlowProjectTemplate/StepDefinitions/BaseStepDefinitions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+using FluentAutomation;
+using TechTalk.SpecFlow;
+
+using $safeprojectname$.Pages.Common;
+
+namespace $safeprojectname$.StepDefinitions
+{
+    [Binding]
+    public sealed class BaseStepDefinition : FluentTest
+    {
+        public BaseStepDefinition()
+        {
+            HomePage = new HomePage(this);
+
+            ConfigureWbTstr();
+        }
+
+        public HomePage HomePage { get; private set; }
+
+        private void ConfigureWbTstr()
+        {
+           WbTstr.Configure()
+                 .UseWebDriver(SeleniumWebDriver.Browser.PhantomJs)
+                 .BootstrapInstance();
+        }
+    }
+}
